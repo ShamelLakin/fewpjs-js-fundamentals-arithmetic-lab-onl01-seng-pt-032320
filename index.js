c92310fbb42e0c06d789df03d7cf2e1dd7550ccb
Shamel Lakin <christlife4u@gmail.com>
2020-10-15 15:39:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+multiply;
+
+let random = Math.random()
+random;
+
+let mod = 19 % 5
+mod;
+
+let max = Math.max(1, 2 , 3 , 7, 20)
+max;
+
